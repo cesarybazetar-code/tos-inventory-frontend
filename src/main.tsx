ec42c049ad4430fa485da0c52e7a0a16daa36a83
cesarybazetar-code <cesarybazetar@gmail.com>
2025-08-29 12:19:00 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-
-/*import React from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './ui/App'
 const root = createRoot(document.getElementById('root')!)
-root.render(React.createElement(App))*/
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const el = document.getElementById("root");
-if (!el) throw new Error("Missing #root in index.html");
-createRoot(el).render(<App />);
+root.render(React.createElement(App))
