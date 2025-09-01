69b05370afbd85d991bbba42519a64063a738aa0
cesarybazetar-code <cesarybazetar@gmail.com>
2025-09-01 19:16:33 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './ui/App'
-const root = createRoot(document.getElementById('root')!)
-root.render(React.createElement(App))
+
+// Grab the root div
+const container = document.getElementById('root') as HTMLElement
+
+// Create root and render App
+ReactDOM.createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
